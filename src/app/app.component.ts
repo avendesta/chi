4e140177f569cd7f65675013d793179c3050b7bf
Aven Desta <avendesta@Avens-Air.attlocal.net>
2024-05-22 07:45:50 -0500

    making sure materialUI works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, MatSlideToggleModule, MatButtonModule],
+  imports: [RouterOutlet, TitleCasePipe, MatSlideToggleModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
