8d39e3ed8e38c53e94582994f548f1fa08c9a849
Aven Desta <avendesta@Avens-Air.attlocal.net>
2024-05-22 07:30:53 -0500

    add material ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import {MatButtonModule} from '@angular/material/button'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe],
+  imports: [RouterOutlet, TitleCasePipe, MatSlideToggleModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
