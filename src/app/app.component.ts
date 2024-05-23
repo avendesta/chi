b1748b04c35fd2acfa52a22e87aa6ba4aad7c3b0
Aven Desta <avendesta@Avens-Air.attlocal.net>
2024-05-22 20:59:30 -0500

    add progess board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,34 @@
 import { TitleCasePipe } from '@angular/common';
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'
-import {MatButtonModule} from '@angular/material/button'
-import {MatIconModule} from '@angular/material/icon'
+import {Component, ViewChild} from '@angular/core';
+import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, MatSlideToggleModule, MatButtonModule, MatIconModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    RouterOutlet, 
+    TitleCasePipe, 
+    MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatCardModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'career-board';
+  @ViewChild(MatAccordion) accordion!: MatAccordion;
 }
